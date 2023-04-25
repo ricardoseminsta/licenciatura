cbe46c7d3e007ce7be80aeb43f9b6e6e4acc3b7c
ricardo <ricardo.emanuel66@gmail.com>
2023-04-24 21:40:36 -0300

    data exercises

--- mobile/typescript/aula_03_input/src/app/tab2/tab2.page.ts
+++ mobile/typescript/aula_03_input/src/app/tab2/tab2.page.ts
@@ -1,16 +1,38 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent, FormsModule]
 })
 export class Tab2Page {
-
+  dia: string;
+  mes: string;
+  ano: string;
+  aviso:string = '';
+  diaSemana: string = '';
   constructor() {}
 
+  calcularDiaSemana(){
+    if(this.dia == undefined || this.mes == undefined || this.ano == undefined ||
+      this.dia.toString().length > 2 || this.mes.toString().length > 2 || this.ano.toString().length > 4 || undefined){
+      this.aviso = 'Digite uma data valida';
+      this.diaSemana = "error - digite uma data valida";
+      console.log(this.dia);
+
+      return;
+    }
+    const diaSemana = new Date(`${this.ano}-${this.mes}-${this.dia}`)
+    const diaSemanaStr = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"]
+    this.diaSemana = diaSemanaStr[diaSemana.getDay()];
+    this.aviso = '';
+    console.log("entrou", diaSemana);
+    console.log("dia tamanho", this.dia.toString().length);
+
+  }
 }
--- mobile/typescript/aula_03_input/src/app/tab3/tab3.page.ts
+++ mobile/typescript/aula_03_input/src/app/tab3/tab3.page.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent, FormsModule],
 })
 export class Tab3Page {
+  data: Date = new Date();
+  dia: number = this.data.getDate();
+  mes: number = this.data.getMonth();
+  ano: number = this.data.getFullYear();
+  hora: number = this.data.getHours();
+  minutos: number = this.data.getMinutes();
+  meses: Array<string> = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+  dataMontada = `${this.dia} de ${this.meses[this.mes]} de ${this.ano}`;
+  horaMontada:string  = this.montarHora(this.hora, this.minutos);
   constructor() {}
+
+  montarHora(hora: number, minutos: number): string {
+    hora < 9 ? "0" + hora : hora;
+    minutos < 9 ? "0" + minutos : minutos;
+
+    return `${hora} horas e ${minutos} minutos`;
+  }
 }
