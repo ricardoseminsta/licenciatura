3b39272c46834cb00c68439dc4feb4a79968f705
Ricardo <ricardo.emanuel66@gmail.com>
2023-08-28 16:31:49 -0300

    comentarios para a apresentação

--- 2023_02/redes/prim.py
+++ 2023_02/redes/prim.py
@@ -19,7 +19,7 @@ for j in range(m):              # ler arestas m
 
 raiz = random.randint(0, n - 1)
 for (x, c) in n_out[raiz]:
-    heapq.heappush(H, (c, raiz, x))
+    heapq.heappush(H, (c, raiz, x)) #  add ao heap um nó sempre marcado e um que não se sabe que está marcado
 
 n_edges = 0
 custo_tot = 0
@@ -27,9 +27,9 @@ marcados = [raiz]  # a raiz inicia marcada
 arv_ger_min = []
 
 while n_edges < n - 1:
-    while True:
+    while True: #  verifica se as duas arestas desse loop estão marcadas
         (c, a, b) = heapq.heappop(H)
-        if b not in marcados:
+        if b not in marcados: #  se não estiver marcado pode seguir
             break
     marcados.append(b)
     print(a, b)
@@ -38,7 +38,7 @@ while n_edges < n - 1:
     arv_ger_min.append((a, b))
     n_edges += 1
     for (x, c) in n_out[b]:
-        if x not in marcados:
+        if x not in marcados: #  aumenta o numero de arestas candidatas
             heapq.heappush(H, (c, b, x))
 
 print(custo_tot)
